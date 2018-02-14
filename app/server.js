017c49b0b452431114621ff6cf42bb83a7858f6c
Jérôme Duban <jduban@enseirb-matmeca.fr>
2018-02-14 08:19:23 +0100

    cors management added

--- app/server.js
+++ app/server.js
@@ -6,6 +6,7 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var ping 		= require('ping');
+var cors 		= require('cors');
 
 var passwordHash = require('password-hash');
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
@@ -25,6 +26,21 @@ app.use(bodyParser.json());
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
+
+// Cors
+var originsWhitelist = [
+  'http://localhost',      //this is my front-end url for development
+  'http://www.myproductionurl.com'
+];
+
+var corsOptions = {
+  origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}
+app.use(cors(corsOptions));
 
 // =======================
 // routes ================
